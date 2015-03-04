be3becec6f95d884d5d2f92c82f51c7ec6af2cff
OstrichRun <347647092@qq.com>
2015-03-04 15:31:39 +0800

    iOS
    拆分

---
+++ src/iOS/DoExt_Device_SM/ext_implement/Do_Device_SM.h
@@ -0,0 +1,14 @@
+//
+//  Do_MyDevice_SM.h
+//  DoExt_SM
+//
+//  Created by @userName on @time.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import "Do_Device_ISM.h"
+#import "doSingletonModule.h"
+
+@interface Do_Device_SM : doSingletonModule<Do_Device_ISM>
+
+@end
