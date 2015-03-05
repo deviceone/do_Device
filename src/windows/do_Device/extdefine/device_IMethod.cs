82f3fb90701298192aaf5e68d87e076683e2ecde
zhouyi <643988878@qq.com>
2015-03-05 18:01:05 +0800

    device

---
+++ src/windows/do_Device/extdefine/device_IMethod.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_Device.extdefine
+{
+    /// <summary>
+    /// 声明自定义扩展组件方法
+    /// </summary>
+    public interface device_IMethod
+    {
+    }
+}
