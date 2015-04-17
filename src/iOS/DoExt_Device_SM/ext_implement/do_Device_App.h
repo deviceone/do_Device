6e51b08db94307776e7c0f68428015a00bd3f618
OstrichRun <347647092@qq.com>
2015-04-17 16:14:36 +0800

    iOS
    添加App代理

---
+++ src/iOS/DoExt_Device_SM/ext_implement/do_Device_App.h
@@ -0,0 +1,14 @@
+//
+//  do_Camera_App.h
+//  DoExt_SM
+//
+//  Created by 刘吟 on 15/4/9.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "doIAppDelegate.h"
+@interface do_Device_App : NSObject<doIAppDelegate>
+
+@end
