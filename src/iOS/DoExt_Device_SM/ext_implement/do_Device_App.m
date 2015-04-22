da0e1ac43db184217a3b8387a61ed82ec713fbde
OstrichRun <347647092@qq.com>
2015-04-22 18:43:42 +0800

    添加app类

---
+++ src/iOS/DoExt_Device_SM/ext_implement/do_Device_App.m
@@ -0,0 +1,36 @@
+//
+//  do_Camera_App.m
+//  DoExt_SM
+//
+//  Created by 刘吟 on 15/4/9.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import "do_Device_App.h"
+
+@implementation do_Device_App
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    return YES;
+}
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    
+}
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    
+}
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    
+}
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    
+}
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    
+}
+@end
